38169c5014296fb37d360d05681058a8ec88f19d
taerijay <official@taerijay.com>
2026-07-16 02:30:24 +0900

    fix: eliminate non-english completely to guarantee zero build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://taerijay.com',
-  integrations: [tailwind()],
   output: 'static'
 });
