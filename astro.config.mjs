af2ae7073f8b1d6997b8fc4d7f6acf8fb9b72d1b
taerijay <official@taerijay.com>
2026-07-16 11:39:02 +0900

    fix: integrate vite alias for named folders and sync critical dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'static'
+  // Vercel 컴파일러가 루트 레벨의components 폴더를 완벽히 추적하도록 에일리어스 인젝션 수립함
+  vite: {
+    resolve: {
+      alias: {
+        '@components': path.resolve('./components'),
+        '@layouts': path.resolve('./src/layouts')
+      }
+    }
+  }
 });
